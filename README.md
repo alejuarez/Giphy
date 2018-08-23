5edecb22aa741b224dcfe00abba5f06c6e34d34f
ale <ale-juarez@hotmail.ca>
2018-08-23 17:03:18 -0400

    Giphy homework completed

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Giphy
-Create a Giphy app
+
+### Create a Giphy app
+
+- This app displays 10 giphy images of Star Wars characters. It also displays the rating.
+- You can add more buttons to the page when you complete the add your search and press submit.
+- Another functionality is that you can click on the image and it will move, click it again and it will stop.
---
+++ assets/javascript/giphy-hw.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+  var starwarsList = ["Leia", "Luke", "Chewbacca"];
+
+  var movie = " star wars";
+  var display = document.getElementById(".container");
+
+  function renderButtons(starwarsList) {
+    $("#buttons").empty();
+    // Looping throgh the array
+    for (var i = 0; i < starwarsList.length; i++) {
+      var a = $("<button>");
+      a.addClass("movie-char");
+      a.attr("data-name", starwarsList[i]);
+      a.text(starwarsList[i]);
+      //  buttons.setAttribute('data-type', starwars);
+      $("#buttons").append(a);
+      //display.append(button);
+      console.log(starwarsList[i]);
+    }
+  }
+
+  function displayMovieInfo() {
+    $("#gifContainer").empty();
+    var movie1 = $(this).attr("data-name");
+    var queryUrl =
+      "http://api.giphy.com/v1/gifs/search?q=" +
+      movie1 +
+      movie +
+      "&api_key=YJvQgvHrxAVUmun9MUVGZSJNPOciKpyI&limit=10&rating=G";
+    console.log("queryurl = ", queryUrl);
+    $.ajax({
+      url: queryUrl,
+      method: "GET"
+    }).then(function(response) {
+      for (var i = 0; i < 10; i++) {
+        var rating = response.data[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var imgURLanimated = response.data[i].images.fixed_width.url;
+        var imgURLstill = response.data[i].images.fixed_width_still.url;
+        var image = $("<img>").attr("src", imgURLstill);
+        image.attr("data-state", "still");
+        image.attr("data-still", imgURLstill);
+        image.attr("data-animate", imgURLanimated);
+        image.addClass("gif");
+        var emptydiv = $("<div>");
+        emptydiv.append(p);
+        emptydiv.append(image);
+        emptydiv.addClass("extra");
+        $("#gifContainer").append(emptydiv);
+      }
+    });
+  }
+
+  renderButtons(starwarsList);
+  $(document).on("click", ".movie-char", displayMovieInfo);
+
+  $(document).on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    console.log(state);
+    console.log($(this).attr("data-animate"));
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+
+  $("#addChar").on("click", function() {
+    var newChar = $("#search-input").val();
+    starwarsList.push(newChar);
+    $("#search-input").val("");
+    renderButtons(starwarsList);
+    return false;
+  });
+});
