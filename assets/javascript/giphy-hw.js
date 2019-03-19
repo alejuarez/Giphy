2c72d6777ea48e90a0b39547f38976c74a16dc98
ale <ale-juarez@hotmail.ca>
2019-03-18 22:47:38 -0400

    fixed it

--- assets/javascript/giphy-hw.js
+++ assets/javascript/giphy-hw.js
@@ -1,77 +1,77 @@
-document.addEventListener("DOMContentLoaded", function(e) {
-  var starwarsList = ["Leia", "Luke", "Chewbacca"];
+document.addEventListener('DOMContentLoaded', function(e) {
+  var starwarsList = ['Leia', 'Luke', 'Chewbacca'];
 
-  var movie = " star wars";
-  var display = document.getElementById(".container");
+  var movie = ' star wars';
+  var display = document.getElementById('.container');
 
   function renderButtons(starwarsList) {
-    $("#buttons").empty();
+    $('#buttons').empty();
     // Looping throgh the array
     for (var i = 0; i < starwarsList.length; i++) {
-      var a = $("<button>");
-      a.addClass("movie-char");
-      a.attr("data-name", starwarsList[i]);
+      var a = $('<button>');
+      a.addClass('movie-char');
+      a.attr('data-name', starwarsList[i]);
       a.text(starwarsList[i]);
       //  buttons.setAttribute('data-type', starwars);
-      $("#buttons").append(a);
+      $('#buttons').append(a);
       //display.append(button);
       console.log(starwarsList[i]);
     }
   }
 
   function displayMovieInfo() {
-    $("#gifContainer").empty();
-    var movie1 = $(this).attr("data-name");
+    $('#gifContainer').empty();
+    var movie1 = $(this).attr('data-name');
     var queryUrl =
-      "https://api.giphy.com/v1/gifs/search?q=" +
+      'https://api.giphy.com/v1/gifs/search?q=' +
       movie1 +
       movie +
-      "&api_key=YJvQgvHrxAVUmun9MUVGZSJNPOciKpyI&limit=10&rating=G";
-    console.log("queryurl = ", queryUrl);
+      '&api_key=YJvQgvHrxAVUmun9MUVGZSJNPOciKpyI&limit=10&rating=G';
+    console.log('queryurl = ', queryUrl);
     $.ajax({
       url: queryUrl,
-      method: "GET"
+      method: 'GET'
     }).then(function(response) {
       for (var i = 0; i < 10; i++) {
         var rating = response.data[i].rating;
-        var p = $("<p>").text("Rating: " + rating);
-        var imgURLanimated = response.data[i].images.fixed_width.url;
-        var imgURLstill = response.data[i].images.fixed_width_still.url;
-        var image = $("<img>").attr("src", imgURLstill);
-        image.attr("data-state", "still");
-        image.attr("data-still", imgURLstill);
-        image.attr("data-animate", imgURLanimated);
-        image.addClass("gif");
-        var emptydiv = $("<div>");
+        var p = $('<p>').text('Rating: ' + rating);
+        var imgURLanimated = response.data[i].images.fixed_height.url;
+        var imgURLstill = response.data[i].images.fixed_height_still.url;
+        var image = $('<img>').attr('src', imgURLstill);
+        image.attr('data-state', 'still');
+        image.attr('data-still', imgURLstill);
+        image.attr('data-animate', imgURLanimated);
+        image.addClass('gif');
+        var emptydiv = $('<div>');
         emptydiv.append(p);
         emptydiv.append(image);
-        emptydiv.addClass("extra");
-        $("#gifContainer").append(emptydiv);
+        emptydiv.addClass('extra');
+        $('#gifContainer').append(emptydiv);
       }
     });
   }
 
   renderButtons(starwarsList);
-  $(document).on("click", ".movie-char", displayMovieInfo);
+  $(document).on('click', '.movie-char', displayMovieInfo);
 
-  $(document).on("click", ".gif", function() {
-    var state = $(this).attr("data-state");
+  $(document).on('click', '.gif', function() {
+    var state = $(this).attr('data-state');
     console.log(state);
-    console.log($(this).attr("data-animate"));
-    if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
+    console.log($(this).attr('data-animate'));
+    if (state === 'still') {
+      $(this).attr('src', $(this).attr('data-animate'));
+      $(this).attr('data-state', 'animate');
     } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
+      $(this).attr('src', $(this).attr('data-still'));
+      $(this).attr('data-state', 'still');
     }
   });
 
-  $("#addChar").on("click", function() {
-    var newChar = $("#search-input").val();
+  $('#addChar').on('click', function() {
+    var newChar = $('#search-input').val();
     if (newChar) {
       starwarsList.push(newChar);
-      $("#search-input").val("");
+      $('#search-input').val('');
       renderButtons(starwarsList);
     }
     return false;
